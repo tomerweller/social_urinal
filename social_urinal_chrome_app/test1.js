d4502abe53fccfc981aabaa9ee207385475eb986
Abhishek Ravi <abhishekravi1992@gmail.com>
2012-09-08 04:32:25 +0800

    Face data posted

--- social_urinal_chrome_app/test1.js
+++ social_urinal_chrome_app/test1.js
@@ -1,5 +1,16 @@
-var xhr = new XMLHttpRequest();
-//xhr.onreadystatechange = handleStateChange; // Implemented elsewhere.
-xhr.open("post","https://lambda-face-recognition.p.mashape.com/album", true);
-xhr.send("album=test");
-console.log(xhr.responseText);
+
+$.get("http://whispering-cove-9010.herokuapp.com/info", function(data){
+console.log("Data Loaded: " + data);
+var token = data.me.access_token;
+var name = data.me.display_name;
+var avatarUrl = data.me.avatar_url;
+var friends = data.friends;
+//postit();
+});
+
+function postit(img)
+{
+console.log("here");
+$.post("whispering-cove-9010.herokuapp.com/urinate", { access_token: token, Quantity: 100ml, displayImage = img } );
+};
+
